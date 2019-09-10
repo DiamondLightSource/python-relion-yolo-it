9f8730c5fe3951b00b908a03b6fd44c64b416481
Webb <donovan.webb@diamond.ac.uk>
2019-09-10 11:44:47 +0100

    Created Fine-Tune cryolo for pipeline

--- external_cryolo_fine.py
+++ external_cryolo_fine.py
@@ -1,6 +1,6 @@
 #!/dls/ebic/data/staff-scratch/Donovan/anaconda/envs/cry_rel/bin/python
 """
-External job for calling cryolo within Relion 3.1
+External job for calling cryolo fine tune within Relion 3.1
 """
 
 import argparse
@@ -19,13 +19,11 @@ def run_job(project_dir, job_dir, args_list):
     print("Job directory is {}".format(job_dir))
     print("arguments are {}".format(args_list))
     parser = argparse.ArgumentParser()
-    parser.add_argument("--in_mics", help="Input micrographs STAR file")
+    parser.add_argument("--in_parts", help="Input micrographs STAR file")
     parser.add_argument("--j", dest="threads", help="Number of threads to run (ignored)")
     parser.add_argument("--box_size", help="Size of box (~ particle size)")
-    parser.add_argument("--threshold", help="Threshold for picking (default = 0.3)")
     args = parser.parse_args(args_list)
     #print(args)
-    thresh = args.threshold
     box_size = args.box_size
     print(box_size)
     with open('/dls_sw/apps/EM/crYOLO/cryo_phosaurus/config.json', 'r') as json_file:
@@ -33,39 +31,45 @@ def run_job(project_dir, job_dir, args_list):
         print(data['model']['anchors'])
         data['model']['anchors'] = [int(box_size), int(box_size)]
         print(data['model']['anchors'])
+        data['train']['pretrained_weights'] = '/dls_sw/apps/EM/crYOLO/cryo_phosaurus/gmodel_phosnet_20190516.h5'
 
     with open('config.json', 'w') as outfile:
         json.dump(data, outfile)
 
-    in_doc = gemmi.cif.read_file(os.path.join(project_dir, args.in_mics))
-    data_as_dict = json.loads(in_doc.as_json())['micrographs']
-    print(data_as_dict.keys())
-    print(data_as_dict['_rlnmicrographname'])
+    in_doc = gemmi.cif.read_file(os.path.join(project_dir, args.in_parts))
+    data_as_dict = json.loads(in_doc.as_json())
+
     try:
-        os.mkdir('cryolo_input')
-    except: print('cryolo_input exists')
-    for micrograph in data_as_dict['_rlnmicrographname']:
-        print(micrograph)
+        os.mkdir('train_annotation')
+    except: 
+        shutil.rmtree('train_annotation')
+        os.mkdir('train_annotation')
+    try:
+        os.mkdir('train_image')
+    except: 
+        shutil.rmtree('train_image')
+        os.mkdir('train_image')
+
+    for micro in range(len(data_as_dict['particles']['_rlnmicrographname'])):
+
         try:
-            os.link(os.path.join(project_dir, micrograph), os.path.join(project_dir, job_dir, 'cryolo_input', os.path.split(micrograph)[-1]))
-        except: print('{} already exists'.format(micrograph))
-        print(os.path.join(project_dir, job_dir, 'cryolo_input', os.path.split(micrograph)[-1]))
-        print(os.path.join(project_dir, micrograph))
+            print(os.path.join(project_dir, data_as_dict['particles']['_rlnmicrographname'][micro]))
+            print(os.path.join(project_dir, job_dir, 'train_image', os.path.split(data_as_dict['particles']['_rlnmicrographname'][micro])[-1]))
+            os.link(os.path.join(project_dir, data_as_dict['particles']['_rlnmicrographname'][micro]), os.path.join(project_dir, job_dir, 'train_image', os.path.split(data_as_dict['particles']['_rlnmicrographname'][micro])[-1]))
+        except: print('{} already exists'.format(micro))
 
-    os.system(f"cryolo_predict.py -c config.json -i {os.path.join(project_dir, job_dir, 'cryolo_input')} -o {os.path.join(project_dir, job_dir, 'gen_pick')} -w /dls_sw/apps/EM/crYOLO/cryo_phosaurus/gmodel_phosnet_20190516.h5 -g 0 -t {thresh}")
-    try:
-        os.mkdir('data')
-    except: print('data file exists')
+        box_name = os.path.splitext(os.path.split(data_as_dict['particles']['_rlnmicrographname'][micro])[-1])[0] + '.box'
+
+        individual_files = open(os.path.join('train_annotation', box_name), 'a+')
+        individual_files.write(f"{data_as_dict['particles']['_rlncoordinatex'][micro] - int(box_size)/2}\t")
+        individual_files.write(f"{data_as_dict['particles']['_rlncoordinatey'][micro] - int(box_size)/2}\t")
+        individual_files.write(f"{box_size}\t")
+        individual_files.write(f"{box_size}\n")
+        individual_files.close()
+
+    os.system(f"cryolo_train.py -c config.json -w 0 -g 0 --fine_tune")
     print('done')
-    for picked in os.listdir(os.path.join(project_dir, job_dir, 'gen_pick', 'STAR')):
-        new_name = os.path.splitext(picked)[0]+'_crypick'+'.star'
-        os.link(os.path.join(project_dir, job_dir, 'gen_pick', 'STAR', picked), os.path.join(project_dir, job_dir, 'data', new_name))
 
-
-    part_doc = open('_crypick.star', 'w')
-    part_doc.write(os.path.join(project_dir, args.in_mics))
-    part_doc.close()
-    
     out_doc = gemmi.cif.Document()
     output_nodes_block = out_doc.add_new_block('output_nodes')
     loop = output_nodes_block.init_loop('', ['_rlnPipeLineNodeName', '_rlnPipeLineNodeType'])
@@ -93,4 +97,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
