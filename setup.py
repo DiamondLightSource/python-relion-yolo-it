32a8500b19ebf8ba4cac64cdb8a26d960139dbb6
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2020-06-01 10:00:36 +0100

    Update setup.py with info from pypiLive branch
    apply black and relax the overly stringent dependency requirements.
    closes #2

--- setup.py
+++ setup.py
@@ -4,12 +4,11 @@
 import sys
 from setuptools import setup, find_packages
 
-requirements = ["gemmi==0.2.8", "numpy==1.14.5"]
+requirements = ["gemmi>=0.2.8", "numpy>=1.14.5"]
 setup_requirements = []
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
-
 
 setup(
     name="relion_yolo_it",
@@ -22,7 +21,7 @@ setup(
     ],
     description="Updated relion_it with cryolo wrappers for Diamond Light Source",
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     install_requires=requirements,
     license="GPLv2",
     include_package_data=True,
@@ -31,11 +30,11 @@ setup(
         "relion_yolo_it/cryolo_relion_it.py",
         "relion_yolo_it/cryolo_pipeline.py",
         "relion_yolo_it/cryolo_external_job.py",
-        "relion_yolo_it/cryolo_fine_tune_job.py"
+        "relion_yolo_it/cryolo_fine_tune_job.py",
     ],
-    python_requires='==3.6.8',
+    python_requires=">=3.6",
     setup_requires=setup_requirements,
+    url="https://github.com/DiamondLightSource/python-relion-yolo-it",
     version="0.4.0",
     zip_safe=False,
 )
-
