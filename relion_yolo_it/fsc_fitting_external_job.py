554394f3d3b721d483e57cd43e36c322b2fac53a
d-j-hatton <daniel.hatton@diamond.ac.uk>
2021-03-30 08:24:17 +0100

    Remove unused numpy and scipy dependencies from fsc_fitting_external_job

--- relion_yolo_it/fsc_fitting_external_job.py
+++ relion_yolo_it/fsc_fitting_external_job.py
@@ -9,12 +9,6 @@ import os
 import os.path
 import shutil
 import time
-import numpy as np
-from scipy.optimize import curve_fit, fsolve, brentq
-import matplotlib
-
-matplotlib.use("Agg")
-import matplotlib.pyplot as plt
 
 import gemmi
 
@@ -39,7 +33,6 @@ def run_job(project_dir, out_dir, fscs_files, args_list):
         data_as_dict = json.loads(fsc_in.as_json())["fsc"]
         invres = [1 / x for x in data_as_dict["_rlnangstromresolution"]]
         fsc = data_as_dict["_rlnfouriershellcorrelationcorrected"]
-        # res = fsc_res(invres, fsc, out_dir, i + 1)
         res = lin_interp(invres, fsc)
         resolutions.append(res)
 
@@ -63,55 +56,6 @@ def crossing_points(invres, fsc):
         if fsc[i] < 0.5 and fsc[i - 1] > 0.5:
             points.append(((invres[i - 1], fsc[i - 1]), (invres[i], fsc[i])))
     return points
-
-
-def fsc_res(invres, fsc, out_dir, iclass):
-    # placeholder function
-    def fitfcn(x, a, b, eps=0):
-        return 1 / (1 + a * np.exp(-b * x ** 2)) - eps
-
-    fitres = curve_fit(fitfcn, invres, fsc, p0=[1, 1])
-    # solution = fsolve(fitfcnsolve, 0.1, args=(fitres[0][0], fitres[0][1]))
-    solution = brentq(fitfcn, 0.001, 1, args=(fitres[0][0], fitres[0][1], 0.5))
-
-    fitrange = np.linspace(0, invres[-1])
-    fitline = [fitfcn(x, fitres[0][0], fitres[0][1]) for x in fitrange]
-
-    subpoints = [(p1, p2) for p1, p2 in zip(invres, fsc) if p2 < 0.7 and p2 > 0.3]
-    subinvres = [p[0] for p in subpoints]
-    subfsc = [p[1] for p in subpoints]
-
-    def linfit(x, a, b, eps=0):
-        return a + b * x - eps
-
-    linfitres = curve_fit(linfit, subinvres, subfsc, p0=[0, 0])
-
-    linfitrange = np.linspace(subinvres[0], subinvres[-1])
-    linfitline = [linfit(x, linfitres[0][0], linfitres[0][1]) for x in linfitrange]
-
-    plt.rc("font", **{"family": "serif", "serif": ["Computer Modern"], "size": 12})
-    plt.rc("text", usetex=True)
-    plt.rc("axes", linewidth=0.5)
-    plt.figure(figsize=((4.5, 4.5 / 1.618)))
-
-    fig, axs = plt.subplots(2)
-
-    axs[0].plot(invres, fsc, "--")
-    axs[0].plot(fitrange, fitline, ":")
-    axs[0].axhspan(0.3, 0.7, alpha=0.5, lw=0)
-    axs[0].axvspan(subinvres[0], subinvres[-1], alpha=0.5, lw=0)
-    axs[0].set_ylabel("FSC")
-
-    axs[1].plot(subinvres, subfsc, "o")
-    axs[1].plot(linfitrange, linfitline, "--")
-    axs[1].set_xlabel("Resolution $\left( \mathrm{\AA}^{-1} \\right)$")
-    axs[1].set_ylabel("FSC")
-
-    plt.tight_layout()
-    plt.savefig(out_dir + f"fsc_curve{iclass}.pdf")
-    plt.close()
-
-    return solution
 
 
 def main():
